d6ae2030b029b869697224f8e88a0c044c091e96
Motorpsycho123 <jhoos@web.de>
2017-11-08 22:34:29 +0100

    changes

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 /**
  *
- * highchart adapter
+ * highcharts adapter
  *
  *
  *  file io-package.json comments:
@@ -9,11 +9,11 @@
  *      "common": {
  *          "name":         "highchart",                  // name has to be set and has to be equal to adapters folder name and main file name excluding extension
  *          "version":      "0.0.0",                    // use "Semantic Versioning"! see http://semver.org/
- *          "title":        "Node.js highchart Adapter",  // Adapter title shown in User Interfaces
+ *          "title":        "Node.js highcharts Adapter",  // Adapter title shown in User Interfaces
  *          "authors":  [                               // Array of authord
  *              "name <mail@highchart.com>"
  *          ]
- *          "desc":         "highchart adapter",          // Adapter description shown in User Interfaces. Can be a language object {de:"...",ru:"..."} or a string
+ *          "desc":         "highcharts adapter",          // Adapter description shown in User Interfaces. Can be a language object {de:"...",ru:"..."} or a string
  *          "platform":     "Javascript/Node.js",       // possible values "javascript", "javascript/Node.js" - more coming
  *          "mode":         "daemon",                   // possible values "daemon", "schedule", "subscribe"
  *          "schedule":     "0 0 * * *"                 // cron-style schedule. Only needed if mode=schedule
@@ -37,7 +37,7 @@ var utils =    require(__dirname + '/lib/utils'); // Get common adapter utils
 // you have to call the adapter function and pass a options object
 // name has to be set and has to be equal to adapters folder name and main file name excluding extension
 // adapter will be restarted automatically every time as the configuration changed, e.g system.adapter.highchart.0
-var adapter = utils.adapter('highchart');
+var adapter = utils.adapter('highcharts');
 
 // is called when adapter shuts down - callback has to be called under any circumstances!
 adapter.on('unload', function (callback) {
